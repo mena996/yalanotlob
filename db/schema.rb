71e0f196ce38efc39004d8f76d628b9bca2fddf9
ahmedatef00 <ahmed62437@student.must.edu.eg>
2020-03-30 12:51:37 +0200

    add name to regestration and edit regestration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_013759) do
+ActiveRecord::Schema.define(version: 2020_03_30_104619) do
 
   create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "first_user_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_013759) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
