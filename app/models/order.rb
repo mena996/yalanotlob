ec9efa46345966981ab1951a03419ff49545b025
shimaaibrahim1 <ir.shaimaa@gmail.com>
2020-04-04 16:14:56 +0200

    modify order-details and add some functionality

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ApplicationRecord
+    
+    has_many :user_orders
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_orders/create'
-  get 'user_orders/destroy'
+  post 'user_orders/create'
+  get 'user_orders/index'
+  delete 'user_orders/destroy'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
