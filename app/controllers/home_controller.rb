75afdb46af482fe4a53e0a754edf2c327d0d5cb3
mena996 <menaemad996@gmail.com>
2020-04-07 01:23:14 +0200

    add add group ui and update link of friends and groups in layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def addFriend 
+    p "heloooooooooooooooooooooo"
+  end 
+  
+  def addGroup
+    p "heloooooooooooooooooooooo"
+  end 
+  
 end
-def addFriend 
-  p "heloooooooooooooooooooooo"
-end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'add_friend', to: 'home#addFriend', as: 'add_friend'
+  get 'add_group', to: 'home#addGroup', as: 'add_group'
   get 'home/index'
   post 'orders/:id/finish_order', to: 'orders#finish', as: 'finish_order'
   resources :orders
