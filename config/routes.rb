4d30165d2f474c6c44c7b5fcab90f9ca2092f132
mena996 <menaemad996@gmail.com>
2020-04-06 22:59:32 +0200

    fix confilict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'user_orders/index'
-  post 'user_orders/create'
   get 'add_friend', to: 'home#addFriend', as: 'add_friend'
   get 'home/index'
   post 'orders/:id/finish_order', to: 'orders#finish', as: 'finish_order'
