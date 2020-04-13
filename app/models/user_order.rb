08fe14a0c175f62e86d7bb08f013a569fd806025
shimaaibrahim1 <ir.shaimaa@gmail.com>
2020-04-13 20:05:22 +0200

    allow only joined friends to add order details

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -5,6 +5,6 @@ class UserOrder < ApplicationRecord
 
     validates :item_name, :amount, :price, presence: true
     validates :item_name, format: {  with: /\A[a-zA-Z0-9]+\z/,message: "please enter available order" }
-    validates :amount, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
-    validates :price, :amount, numericality: { greater_than_or_equal_to: 0 }
+    validates :amount, numericality: { only_integer: true , greater_than: 0 }
+    validates :price, :amount, numericality: { greater_than: 0 }
 end
